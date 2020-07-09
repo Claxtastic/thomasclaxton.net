f2c646e328272e7747995c83338e9605d9236bbf
claxtastic <claxtons4@gmail.com>
2020-07-09 13:02:15 -0500

    Everything seems to be working with S3 locally, will deploy to Elastic Beanstalk

--- my_site/settings.py
+++ my_site/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'apps.blog',
     'apps.home',
     'apps.about',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -136,9 +137,26 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# AWS S3
+AWS_ACCESS_KEY_ID = secrets.AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY = secrets.AWS_SECRET_ACCESS_KEY
+AWS_STORAGE_BUCKET_NAME = 'claxtastic-static'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl' : 'max-age=86400',
+}
+
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
+
+DEFAULT_FILE_STORAGE = 'my_site.storage_backends.MediaStorage'
