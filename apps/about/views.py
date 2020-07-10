74e8c1c2c9cb1ac407e645828f453cf425d00293
claxtastic <claxtons4@gmail.com>
2020-07-10 12:41:25 -0500

    Added resume to /about

--- apps/about/views.py
+++ apps/about/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from apps.about.models import Resume
 
 # Create your views here.
 
 def about(request):
-    return render(request, "about.html")
+    # get most recently uploaded resume
+    # resume = Resume.objects.all().order_by('-id')[0]
+    resume = Resume.objects.last()
+    context = {
+        'resume' : resume
+    }
+
+    return render(request, "about.html", context)
